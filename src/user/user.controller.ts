422b9d64202bf03d0a5d0fbc22a373ed5de90150
izaelgs <izael-123@outlook.com>
2023-12-10 19:58:45 -0300

    modifica dtos user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,12 +6,14 @@ import {
   Patch,
   Param,
   Delete,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+  Put,
+} from "@nestjs/common";
+import { UserService } from "./user.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdatePatchUserDto } from "./dto/update-patch-user.dto";
+import { UpdatePutUserDto } from "./dto/update-put-user.dto copy";
 
-@Controller('user')
+@Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -25,18 +27,23 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.userService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Put(":id")
+  put(@Param("id") id: string, @Body() updateUserDto: UpdatePutUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdatePatchUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.userService.remove(+id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-patch-user.dto';
 
 @Injectable()
 export class UserService {
