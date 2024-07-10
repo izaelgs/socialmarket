d53bd91d7a645da432a68a21b6e1635a6daef002
izaelgs <izael-123@outlook.com>
2024-07-10 11:04:18 -0300

    implements cors configuration

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
+
+  const corsOptions: CorsOptions = {
+    origin: [/\.iza\.dev\.br$/, "https://iza.dev.br"],
     credentials: true,
-  });
+  };
+
+  app.enableCors(corsOptions);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
+
 bootstrap();
